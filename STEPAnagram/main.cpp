d169e5b37f8d2df9ec3a1c1adb105d8d5264ef9e
Amy McNeill <amymcneill@Amys-MacBook-Pro.local>
2016-06-02 22:10:42 +0900

    Bug from double letter handling

--- STEPAnagram/main.cpp
+++ STEPAnagram/main.cpp
@@ -1,15 +1,118 @@
-//
-//  main.cpp
-//  STEPAnagram
-//
-//  Created by Amy McNeill on 6/2/16.
-//  Copyright (c) 2016 Amy McNeill. All rights reserved.
-//
-
 #include <iostream>
+#include <fstream>
+#include <string> 
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+const string dictionaryLocation = "//Users//amymcneill//Desktop//AnagramProject//AnagramProject//dictionary.txt";
+
+
+int main()
+{
+    //string variables
+    string inputWord = "";
+    string dictionaryWord = "";
+    string winner = "";
+    
+    //variables for exception handling
+    unsigned long notSixteen = 111111;
+    long wordTooLong = 111;
+    unsigned char noWord;
+    
+    try
+    {
+        //gather user input for 16 letters
+        cout << "Please enter exactly 16 letters: ";
+        cin >> inputWord;
+        
+        //exception handling
+        if (inputWord.length() != 16)
+            throw notSixteen;
+            
+    
+        //open the dictionary
+        fstream inFileStream;
+        inFileStream.open(dictionaryLocation.c_str());
+        while(inFileStream)
+        {
+            try
+            {
+               inFileStream >> dictionaryWord;
+            
+               if(dictionaryWord.length() > 16)
+                   throw wordTooLong;
+                
+               else if(dictionaryWord == inputWord)
+               {
+                   winner = dictionaryWord;
+                   break;
+               }
+                
+               else
+               {
+                   //boolean array to avoid invalid double letter
+                   bool inputBin[15];
+                   
+                   for(int ii = 0; ii < 16; ii++)
+                   {
+                       inputBin[ii] = false;
+                   }
+                   
+                   //binary array to ensure every letter is in the input
+                   bool* binary = new bool[dictionaryWord.length()];
+                   for (int ii = 0; ii < dictionaryWord.length(); ii++)
+                   {
+                       binary[ii] = false;
+                   }
+                   
+                   for(int ii = 0; ii < dictionaryWord.length(); ii++)
+                   {
+                       for(int xx = 0; xx < 15; xx++)
+                       {
+                          if(inputBin[xx] == false && dictionaryWord[ii] == inputWord[xx])
+                          {
+                              binary[ii] = true;
+                              inputBin[xx] = true;
+                          }//END if statement for new match found
+                       }//END for loop for comparison of each letter (inner)
+                   }//END for loop for comparison of each letter (outer)
+                   
+                   bool flag = false;
+                   for (int ii = 0; ii < dictionaryWord.length(); ii++)
+                   {
+                       if(!(binary[ii]))
+                           flag = true;
+                   }
+                   delete binary;
+                   
+                   if(flag == false && dictionaryWord.length() > winner.length())
+                       winner = dictionaryWord;
+               }
+            }//END try block for wordTooLong
+            
+            catch(long)
+            {
+                //nothing to do here, just for efficiency
+            }//END catch block for wordTooLong
+        
+        }//END while loop for exception handling
+        
+        //exception handling for no word found
+        if (winner == "not a word")
+            throw noWord;
+        
+        //announce the winner
+        cout << "The longest word composed of only your sixteen letters is: " << winner << endl;
+    }//END try block for notSixteen and noWord
+    
+    //exception handling
+    catch(unsigned long)
+    {
+        cout << "I told you to put in 16 letters!" << endl;
+    }
+    
+    catch(unsigned char)
+    {
+        cout << "I'm sorry, those letters make absolutely no words." << endl;
+    }
     return 0;
-}
+}//END function main
